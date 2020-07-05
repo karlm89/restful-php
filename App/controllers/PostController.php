d543dd211a3920b8e81f6b4154416f1ad0c16c2a
Karl Murray <karl@fwdmotion.studio>
2020-07-05 10:50:29 -0500

    Building out basic file structure

---
+++ App/controllers/PostController.php
@@ -0,0 +1,27 @@
+<?php 
+    class PostController {
+        public function __constuct () {
+
+        }
+        public function index () {
+
+        }
+        public function create () {
+
+        }
+        public function store () {
+
+        }
+        public function show () {
+
+        }
+        public function edit () {
+
+        }
+        public function update () {
+
+        }
+        public function destroy () {
+
+        }
+    }
